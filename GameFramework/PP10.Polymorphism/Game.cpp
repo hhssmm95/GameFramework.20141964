4052ec556e1bb21fb12b70214e7f983114bea90e
SeungJun Han <hhssmm95@naver.com>
2018-10-29 20:20:35 +0900

    PP10.Polymorphism 미완 2018-10-29

--- GameFramework/PP10.Polymorphism/Game.cpp
+++ GameFramework/PP10.Polymorphism/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include "TextureManager.h"
+#include "Enemy.h"
 bool Game::init(const char* title, int xpos, int ypos,
 	int width, int height, bool fullscreen)
 {
@@ -28,6 +29,7 @@ bool Game::init(const char* title, int xpos, int ypos,
 		return false;
 	}
 	m_gameObjects.push_back(new Player(new LoaderParams(100, 100, 128, 82, "animate")));
+	m_gameObjects.push_back(new Enemy(new LoaderParams(300, 300, 128, 82, "animate")));
 
 	m_go.load(100, 100, 128, 82, "animate");
 	m_player.load(300, 300, 128, 82, "animate");
