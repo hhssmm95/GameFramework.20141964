c7ea2415af23a800d46be99b7a9ce928c5763666
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-11-09 12:16:11 +0900

    PP12완성, PP13.KInputHandler, PP14.MInputHandler 2018-11-09 (PP14수정해야함)

---
+++ GameFramework/PP14.MInputHandler/Game.cpp
@@ -0,0 +1,87 @@
+#include "Game.h"
+#include "InputHandler.h"
+#include "TextureManager.h"
+#include "Enemy.h"
+
+
+InputHandler* InputHandler::s_pInstance = 0;
+
+bool Game::init(const char* title, int xpos, int ypos,
+	int width, int height, bool fullscreen)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+
+		if (m_pWindow != 0)
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+		m_bRunning = true;
+
+
+	}
+
+	else {
+		return false;
+	}
+
+	if (!TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
+	{
+
+		std::cout << "TMload error";
+
+		return false;
+	}
+
+
+	m_gameObjects.push_back(new Player(new LoaderParams(100, 100, 128, 82, "animate")));
+	m_gameObjects.push_back(new Enemy(new LoaderParams(300, 300, 128, 82, "animate")));
+
+
+	SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
+
+	return true;
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	for (std::vector<GameObject*>::size_type i = 0;
+		i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->draw();
+	}
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::update()
+{
+	for (std::vector<GameObject*>::size_type i = 0;
+		i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->update();
+	}
+}
+
+
+void Game::clean()
+{
+
+	std::cout << "cleaning game\n";
+	TheInputHandler::Instance()->clean();
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::handleEvents()
+{
+	TheInputHandler::Instance()->update();
+
+}
+
+void Game::quit()
+{
+	clean();
+}
